d58018ccda8c6ce38e1bd82e0748570673e77425
Huiming Teo <teohuiming@gmail.com>
2013-04-12 14:36:48 +0800

    add postgres ACL config

--- recipes/postgresql.rb
+++ recipes/postgresql.rb
@@ -15,6 +15,15 @@ include_recipe "postgresql::client"
 include_recipe "postgresql::server"
 include_recipe "database::postgresql"
 
+#TODO Chef 11 compat?
+node.set['postgresql']['pg_hba'] = [
+  {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
+  {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'md5'},
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
+  {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'}
+]
+
+
 postgresql_connection_info = {
   :host => "localhost",
   :port => node['postgresql']['config']['port'],
